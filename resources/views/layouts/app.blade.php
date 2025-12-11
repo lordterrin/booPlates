ccf950f3ca61913ab3f27bbbcebaa5fc8f1a2e25
BP <lordterrin@gmail.com>
2025-12-10 17:59:10 -0800

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'BooPlates')</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+
     <!-- Global CSS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
